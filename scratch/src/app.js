70dba5d291cc965f7a42eac95df5ad7f1c878068
Adam Bien <abien@adam-bien.com>
2019-11-27 09:56:13 +0100

    simplest possible component

--- scratch/src/app.js
+++ scratch/src/app.js
@@ -1,6 +1,27 @@
 class Hello extends HTMLElement { 
+
+    constructor() { 
+        super();
+        this.state = {
+            message: 'god morgen'
+        };
+    }
+
+    connectedCallback() { 
+        const { message } = this.state;
+        this.innerHTML = `
+            <h2>${message}</h2>
+        `
+
+    }
     
+    disconnectedCallback() { 
+
+    }
+
 }
+customElements.define('a-hello',Hello);
+
 
 
 
