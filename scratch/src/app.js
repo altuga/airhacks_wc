059ba31d72ea5ad76a50cf91c7f49cf9c38d11fb
Adam Bien <abien@adam-bien.com>
2019-11-27 10:36:42 +0100

    dom diffing

--- scratch/src/app.js
+++ scratch/src/app.js
@@ -1,3 +1,5 @@
+const $ = q => document.querySelectorAll(q);
+
 import { html, render } from './lib/lit-html/lit-html.js';
 import './sample.js';
 class Hello extends HTMLElement { 
@@ -10,18 +12,24 @@ class Hello extends HTMLElement {
     }
 
     connectedCallback() { 
+        this.viewChanged();
+        setInterval(_ => this.viewChanged(), 200);
+    }
+
+    viewChanged() { 
         const { messages } = this.state;
         
         const template = html`
             <button @click=${_ => this.clicked()}>click</button>
             <input placeholder="something" @change=${e => this.changed(e)}>
             <ol>
-            ${messages.map(m => html`<li>${m}</li>`)}
+            ${messages.map(m => html`<li>${m} ${new Date()}</li>`)}
              </ol>
         `
         render(template,this);
 
     }
+
     changed(event) { 
         console.log(event);
     }
